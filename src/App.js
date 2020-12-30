4ce8adea095aa2aee46d0b723b3e924d16889a56
NikitOs <exemplum777@gmail.com>
2020-12-30 16:57:33 +0300

    Implements full css style

--- src/App.js
+++ src/App.js
@@ -6,50 +6,57 @@ function App() {
     <div className="container">
       <h2 className="title">Password generator</h2>
 
-      <div className="result_title field-title">
-        Generated password
-      </div>
-      <div className="result_info right">
-        click to copy
-      </div>
-      <div className="result_info left">
-        copied 
-      </div>
+      <div className="result">
+        <div className="result_title field-title">
+          Generated password
+        </div>
+        <div className="result_info right">
+          click to copy
+        </div>
+        <div className="result_info left">
+          copied 
+        </div>
 
-      <div className="result_viewbox" id="result">
-        click to generate
-      </div>
+        <div className="result_viewbox" id="result">
+          click to generate
+        </div>
 
-      <button id="copy-btn">
-        {/* COPY ICON */}
-        {/* <img src="" alt=""/> */}
-      </button>
+        <button id="copy-btn">
+          <img src={'https://img.icons8.com/dusk/64/000000/copy.png'} width="70%" alt="Copy Icon"/>
+        </button>
+      </div>
 
       <div className="length range_slider" data-min="4" data-max="32">
         <div className="length_title field-title" data-length="0">
           length: 
         </div>
-        <input type="range" id="slider" min="4" max="32" value="16" step="1" />
+        <input type="range" id="slider" min="4" max="32" step="1" />
       </div>
 
-      <div className="setting">
+      <div className="settings">
         <span className="setting_title field-title">
           setting
         </span>
-        <div className="set">
-          <input type="checkbox" id="uppercase" checked />
+        <div className="setting">
+          <input type="checkbox" id="uppercase" />
           <label for="uppercase">
             Include Uppercase
           </label>
         </div>
-        <div className="set">
-          <input type="checkbox" id="lowercase" checked />
+        <div className="setting">
+          <input type="checkbox" id="lowercase" />
           <label for="lowercase">
             Include Lowercase
           </label>
         </div>
-        <div className="set">
-          <input type="checkbox" id="number" checked />
+        <div className="setting">
+          <input type="checkbox" id="number" />
+          <label for="number">
+            Include Numbers
+          </label>
+        </div>
+        <div className="setting">
+          <input type="checkbox" id="symbol" checked />
           <label for="number">
             Include Symbols
           </label>
